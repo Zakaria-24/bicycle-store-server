03a28a37b0d478c36d636f20c923da379ab8041b
Zakaria-24 <zakariajoarder.bd@gmail.com>
2024-11-29 06:11:38 +0600

    solved: path problem and update: package.json

--- src/app.ts
+++ src/app.ts
@@ -2,27 +2,21 @@ import express, { Request, Response } from 'express'
 import productRouter from './modules/product/product.router'
 import orderRouter from './modules/order/order.router'
 import globalErrorHandler from './utils/globalErrorHandler'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 // POST=> '/api/products/create-order'
 app.use('/api/products', productRouter)
 app.use('/api/orders', orderRouter)
 app.use(globalErrorHandler)
 
-app.use((req, res) => {
-  res.json({
-    status: 404,
-    success: false,
-    message: 'Route not found',
-  })
-})
-
 app.get('/', (req: Request, res: Response) => {
   res.send({
     status: true,
-    message: 'Welcome to the Express.js API!',
+    message: 'Welcome to the Bi-Cycle SERVER!',
   })
 })
 
