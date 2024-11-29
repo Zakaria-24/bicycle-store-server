01d0d8e67798e9440ec1c3c5fede987d5af32928
Zakaria-24 <zakariajoarder.bd@gmail.com>
2024-11-29 06:31:03 +0600

    removed: extra comment out code

--- src/utils/globalErrorHandler.ts
+++ src/utils/globalErrorHandler.ts
@@ -1,22 +1,3 @@
-// import { ErrorRequestHandler } from 'express'
-// import config from '../config'
-
-// const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
-//   const statusCode = err.statusCode || 500
-//   const message = err.message || 'Internal Server Error'
-
-//   res.json({
-//     status: statusCode,
-//     success: false,
-//     message,
-//     stack: config.NODE_ENV === 'development' ? err.stack : undefined, // for development mode.
-//   })
-
-//   console.error(err)
-// }
-
-// export default globalErrorHandler
-
 import { ErrorRequestHandler } from 'express'
 import config from '../config'
 
