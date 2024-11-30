4e113049304301380e07b3d20c372bdd42339cfe
Zakaria-24 <zakariajoarder.bd@gmail.com>
2024-11-30 18:30:10 +0600

    update order creation logic

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -2,11 +2,50 @@
 import { Request, Response } from 'express'
 import { orderService } from './order.service'
 import config from '../../config'
+import { productService } from '../product/product.service'
 
 const createOrder = async (req: Request, res: Response) => {
   try {
+    // const payload = req.body
     const payload = req.body
-    const result = await orderService.createOrder(payload)
+    const { email, product, quantity } = payload
+
+    const bicycle = await productService.getSingleProduct(product)
+
+    if (!bicycle) {
+      return res.status(404).json({
+        status: false,
+        message: 'Bicycle not found',
+      })
+    }
+
+    if (bicycle?.quantity < quantity) {
+      return res.status(404).json({
+        status: false,
+        message: `Insufficient stock. Only ${bicycle.quantity} item(s) left.`,
+      })
+    }
+
+    if (bicycle.quantity == 0) {
+      return res.status(404).json({
+        status: false,
+        message: 'order at lest one',
+      })
+    }
+
+    const totalPrice = bicycle.price * quantity
+
+    // Update product stock
+    bicycle.quantity -= quantity //bicycle.quantity = bicycle.quantity - quantity
+    bicycle.inStock = bicycle.quantity > 0
+    await bicycle.save()
+
+    const result = await orderService.createOrder({
+      email,
+      product,
+      quantity,
+      totalPrice,
+    })
 
     res.json({
       status: true,
--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -1,37 +1,37 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import Product from '../product/product.model'
+// import Product from '../product/product.model'
 import { IOrder } from './order.interface'
 import Order from './order.model'
 
 const createOrder = async (payload: IOrder): Promise<IOrder> => {
-  const { email, product, quantity } = payload
+  // const { email, product, quantity } = payload
 
   // Fetch product details(main product that will be ordered)
-  const bicycle = await Product.findById(product)
-  if (!bicycle) {
-    throw new Error('Product not found')
-  }
+  // const bicycle = await Product.findById(product)
+  // if (!bicycle) {
+  //   throw new Error('Product not found')
+  // }
 
-  if (bicycle.quantity == 0) {
-    throw new Error('order at lest one')
-  }
+  // if (bicycle.quantity == 0) {
+  //   throw new Error('order at lest one')
+  // }
 
   // Validate stock(order product er quantity jodi main j product{bicycle.quantity} ache tar quantity er cheye beshi hoy tahole Insufficient stock  )
-  if (bicycle.quantity < quantity) {
-    throw new Error('Insufficient stock')
-  }
+  // if (bicycle.quantity < quantity) {
+  //   throw new Error('Insufficient stock')
+  // }
 
   // Calculate total price
-  const totalPrice = bicycle.price * quantity
+  // const totalPrice = bicycle.price * quantity
 
-  const order = await Order.create({ email, product, quantity, totalPrice })
+  const result = await Order.create(payload)
 
-  // Update product stock
-  bicycle.quantity -= quantity //bicycle.quantity = bicycle.quantity - quantity
-  bicycle.inStock = bicycle.quantity > 0
-  await bicycle.save()
+  // // Update product stock
+  // bicycle.quantity -= quantity //bicycle.quantity = bicycle.quantity - quantity
+  // bicycle.inStock = bicycle.quantity > 0
+  // await bicycle.save()
 
-  return order
+  return result
 }
 
 const calculateRevenue = async () => {
